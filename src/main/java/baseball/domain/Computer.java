725fd24682802773c74155a6274f245e16d9a963
wogus <ind07152@naver.com>
2024-12-09 22:59:20 +0900

    feat(Computer): 중복 숫자에 대한 검증 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -3,14 +3,16 @@ package baseball.domain;
 import baseball.constants.GameConstant;
 import baseball.exception.ErrorMessage;
 import baseball.exception.GameException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Computer {
     private final List<Number> numbers;
 
     public Computer(List<Integer> numbers) {
-        Validator.validateNumbersSize(numbers);
+        Validator.validateComputerNumber(numbers);
         this.numbers = numbers.stream().map(Number::new).toList();
     }
 
@@ -19,9 +21,21 @@ public class Computer {
     }
 
     static class Validator {
+        private static void validateComputerNumber(List<Integer> numbers) {
+            validateNumbersSize(numbers);
+            validateDuplicate(numbers);
+        }
+
         private static void validateNumbersSize(List<Integer> numbers) {
             if (numbers.size() != GameConstant.NUMBERS_SIZE.getNumber()) {
                 throw GameException.from(ErrorMessage.INVALID_NUMBER_SIZE);
+            }
+        }
+
+        private static void validateDuplicate(List<Integer> numbers) {
+            Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+            if (uniqueNumbers.size() != numbers.size()) {
+                throw GameException.from(ErrorMessage.INVALID_NUMBER_DUPLICATE);
             }
         }
     }
