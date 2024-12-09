67ac9e80bdec5e1dbcc74b2956335209a74182ce
wogus <ind07152@naver.com>
2024-12-09 21:13:13 +0900

    feat(Parser): 재시작에 대한 입력 결과 도출 기능 추가

--- src/main/java/baseball/utils/Parser.java
+++ src/main/java/baseball/utils/Parser.java
@@ -4,12 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
-    public static List<Integer> splitNumber(String input) {
+    public static List<Integer> splitNumbers(String input) {
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < input.length(); i++) {
             int number = Integer.parseInt(String.valueOf(input.charAt(i)));
             numbers.add(number);
         }
         return numbers;
+    }
+
+    public static boolean getRestartResult(String input) {
+        int result = Integer.parseInt(input);
+        return result == 1;
     }
 }
