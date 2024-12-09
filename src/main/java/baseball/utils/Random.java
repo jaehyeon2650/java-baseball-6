99256bc705e6ec907c36d4574868c1b0560a59da
wogus <ind07152@naver.com>
2024-12-09 21:15:46 +0900

    feat(Random): 문제 정답 생성 기능 추가
    - 중복되지 않은 세 숫자를 생성한다.

---
+++ src/main/java/baseball/utils/Random.java
@@ -0,0 +1,18 @@
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Random {
+    public static List<Integer> makeAnswer() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+}
