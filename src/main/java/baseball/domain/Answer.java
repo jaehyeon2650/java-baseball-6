db58197ecee443031dd8496f39734ed40d214faf
wogus <ind07152@naver.com>
2024-12-09 22:12:45 +0900

    feat(Answer): 숫자 개수에 대한 검증 기능 추가
    숫자가 3개인지 검증하는 기능을 추가한다.

--- src/main/java/baseball/domain/Answer.java
+++ src/main/java/baseball/domain/Answer.java
@@ -1,15 +1,18 @@
 package baseball.domain;
 
+import baseball.exception.ErrorMessage;
+import baseball.exception.GameException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Answer {
+    private static final Integer NUMBERS_SIZE = 3;
     private final List<Number> numbers;
 
     public Answer(List<Integer> numbers) {
-        Validator.validateDuplicate(numbers);
+        Validator.validateAnswerNumber(numbers);
         this.numbers = numbers.stream().map(Number::new).toList();
     }
 
@@ -18,10 +21,21 @@ public class Answer {
     }
 
     static class Validator {
+        private static void validateAnswerNumber(List<Integer> numbers) {
+            validateNumbersSize(numbers);
+            validateDuplicate(numbers);
+        }
+
         private static void validateDuplicate(List<Integer> numbers) {
             Set<Integer> uniqueNumbers = new HashSet<>(numbers);
             if (uniqueNumbers.size() != numbers.size()) {
                 throw new IllegalArgumentException();
+            }
+        }
+
+        private static void validateNumbersSize(List<Integer> numbers) {
+            if (numbers.size() != NUMBERS_SIZE) {
+                throw GameException.from(ErrorMessage.INVALID_NUMBER_SIZE);
             }
         }
     }
