472d31177a4cdc82ccb68bab189acf836f837092
wogus <ind07152@naver.com>
2024-12-09 21:26:59 +0900

    feat(Answer): 중복 숫자에 대한 검증 기능 추가
    Answer 객체를 생성할때 중복 숫자가 있는지 검증하는 기능을 추가한다.

--- src/main/java/baseball/domain/Answer.java
+++ src/main/java/baseball/domain/Answer.java
@@ -1,17 +1,28 @@
 package baseball.domain;
 
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Answer {
     private final List<Number> numbers;
 
     public Answer(List<Integer> numbers) {
+        Validator.validateDuplicate(numbers);
         this.numbers = numbers.stream().map(Number::new).toList();
-
     }
 
     public List<Number> getNumbers() {
         return Collections.unmodifiableList(numbers);
+    }
+
+    static class Validator {
+        private static void validateDuplicate(List<Integer> numbers) {
+            Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+            if (uniqueNumbers.size() != numbers.size()) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
