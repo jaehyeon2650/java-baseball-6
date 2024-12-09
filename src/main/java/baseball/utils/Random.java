d90f8313cdcca4e21c19267aefc55ad75ff49c9f
wogus <ind07152@naver.com>
2024-12-09 22:24:44 +0900

    refactor(Random): 게임 관련 숫자 상수화

--- src/main/java/baseball/utils/Random.java
+++ src/main/java/baseball/utils/Random.java
@@ -1,5 +1,6 @@
 package baseball.utils;
 
+import baseball.constants.GameConstant;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,8 +8,9 @@ import java.util.List;
 public class Random {
     public static List<Integer> makeAnswer() {
         List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (numbers.size() < GameConstant.NUMBERS_SIZE.getNumber()) {
+            int randomNumber = Randoms.pickNumberInRange(GameConstant.MIN_NUMBER.getNumber(),
+                    GameConstant.MAX_NUMBER.getNumber());
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
             }
