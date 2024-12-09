dad5503a78d6a67f4fe1178580aa1a1242a81899
wogus <ind07152@naver.com>
2024-12-09 22:14:26 +0900

    refactor(InputView): 커스텀 예외로 교체
    기존의 예외를 커스텀 예외로 교체한다.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.exception.ErrorMessage;
+import baseball.exception.GameException;
 import baseball.utils.Parser;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
@@ -15,51 +17,37 @@ public class InputView {
     public static boolean restart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String number = Console.readLine().trim();
-        Validator.validateNumberType(number);
-        Validator.validateRestartNumberRange(number);
+        Validator.validateRestartInput(number);
         return Parser.getRestartResult(number);
     }
 
     static class Validator {
         private static void validateGameInput(String input) {
             validateLength(input);
-            validateNumber(input);
+            validateNumberType(input);
+        }
+
+        private static void validateRestartInput(String input) {
+            Validator.validateNumberType(input);
+            Validator.validateRestartNumberRange(input);
         }
 
         private static void validateLength(String input) {
             if (input.length() != 3) {
-                throw new IllegalArgumentException();
+                throw GameException.from(ErrorMessage.INVALID_NUMBER_SIZE);
             }
-        }
-
-        private static void validateNumber(String input) {
-            validateNumberRange(input);
-            validateNumberType(input);
         }
 
         private static void validateNumberType(String input) {
             if (!input.matches("\\d+")) {
-                throw new IllegalArgumentException();
-            }
-        }
-
-        private static void validateNumberRange(String input) {
-            try {
-                for (int i = 0; i < input.length(); i++) {
-                    int number = Integer.parseInt(String.valueOf(input.charAt(i)));
-                    if (number <= 0 || number > 9) {
-                        throw new IllegalArgumentException();
-                    }
-                }
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException();
+                throw GameException.from(ErrorMessage.INVALID_NUMBER_TYPE);
             }
         }
 
         private static void validateRestartNumberRange(String input) {
             int number = Integer.parseInt(input);
             if (number != 1 && number != 2) {
-                throw new IllegalArgumentException();
+                throw GameException.from(ErrorMessage.INVALID_RESTART_VALUE);
             }
         }
     }
